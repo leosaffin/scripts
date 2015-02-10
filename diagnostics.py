babe80aeffcfc7b425bed95c053a6c94ecd46ca3
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-02-10 16:44:24 +0000

    Made a better function for calculating PV dipole

--- diagnostics.py
+++ diagnostics.py
@@ -27,6 +27,7 @@ def tropopause(pv,altitude,res=2500):
                                                     mask[:,j,i],res)
     return mask        
 
+# Perform a downward search for tropopause
 def search(pv,pvtrop,z,k,zmask,res):
     test = 0
     while(test==0 and z[k]>2e3):
@@ -38,22 +39,14 @@ def search(pv,pvtrop,z,k,zmask,res):
         zmask = np.logical_or(np.abs(z-ztrop)<=res, zmask==1).astype(int)
     return[test, k, zmask]
 
-# Calculate 
-def weighted_average(weight,bins,diags,nbins):
-    count = np.zeros((len(diags),nbins))
-    total_weight = np.zeros(nbins)
+def dipole(cube,pv,bins,weights):
+    # Calculate the weighted average for each bin
+    mean = np.zeros(len(bins))
     # Loop over bins
-    for n in xrange(0,nbins):
-        # Take the array where it is in the bins
-        weight_mask = np.ma.masked_where(bins!=n, weight)
-        # Sum the weight inside the bins
-        total_weight[n] = np.sum(weight_mask)
-        # Loop over diagnostics
-        for nn,diag in enumerate(diags):
-            # Integrate the weighted diagnostic over all bins
-            count[nn,n] = np.sum(diag*weight_mask)
-    # Loop over diagnostics
-    for nn in xrange(0,len(diags)):
-        # Normalise by total weight
-        count[nn,:] = count[nn,:]/total_weight
-    return count
+    for n,[bin_min,bin_max] in enumerate(bins):
+        # Mask for PV not in bin
+        weight_mask = np.ma.masked_where(pv<bin_min, weights)
+        weight_mask = np.ma.masked_where(pv>bin_max, weight_mask)
+        mass = np.ma.sum(weight_mask)
+        mean[n] = np.ma.sum(cube.data*weight_mask)/mass
+    return mean
