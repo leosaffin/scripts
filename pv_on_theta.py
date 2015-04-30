ac1bb06472d94814190d5642dda516566fa17a65
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-04-30 11:44:19 +0000

    PV on theta working

--- pv_on_theta.py
+++ pv_on_theta.py
@@ -5,17 +5,21 @@ from mymodule import io, convert, grid, interpolate, plot
 
 def main(filename, name, theta_value):
     cubes = io.load(filename)
+    cubes.remove(cubes.extract('air_pressure')[0])
     x = convert.calc(name, cubes)
 
     pv = convert.calc('total_pv', cubes)
     theta = convert.calc('air_potential_temperature', cubes)
     thcoord = grid.make_coord(theta)
     pv.add_aux_coord(thcoord, [0, 1, 2])
+    x.add_aux_coord(thcoord, [0, 1, 2])
 
-    pv_on_theta = interpolate.to_level(pv, theta=theta_value)
+    pv_on_theta = interpolate.to_level(pv, 'air_potential_temperature',
+                                       theta_value)
     pv_on_theta = pv[0].copy(data=pv_on_theta)
 
-    x_on_theta = interpolate.to_level(pv, theta=theta_value)
+    x_on_theta = interpolate.to_level(pv, 'air_potential_temperature',
+                                      theta_value)
     x_on_theta = x[0].copy(data=x_on_theta)
 
     io.save(pv_on_theta, '/home/lsaffi/data/' + name + '_on_theta_' +
@@ -31,5 +35,4 @@ if __name__ == '__main__':
     filename = '/projects/diamet/lsaffi/xjjhq/*030'
     name = 'advection_inconsistency_pv'
     theta_value = 330
-    main(filename)
-
+    main(filename, name, theta_value)
