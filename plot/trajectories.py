b9e401539f067b5b0b5009e58cc59fc451698702
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-05-14 13:02:51 +0000

    Changed plot trajectories to plot what is going in the paper

--- plot/trajectories.py
+++ plot/trajectories.py
@@ -1,43 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cPickle as pickle
-import load
-import interpolate
+from mymodule import files, convert, grid, interpolate, plot
 
 # Load RDF PV
-with open('/home/lsaffi/data/rdf_pv.pkl','rb') as infile:
+with open('/home/lsaffi/data/rdf_pv.pkl', 'rb') as infile:
     rdf_pv = pickle.load(infile)
 
 # Load final time PV tracers
-cubes = load.all('xjjho',35)
-adv = cubes.extract('Advection Only PV')[0].data
-diab = (cubes.extract('Atmospheric Physics 1 PV')[0].data + 
-        cubes.extract('Atmospheric Physics 2 PV')[0].data)
-other = (cubes.extract('Cloud Rebalancing PV')[0].data + 
-         cubes.extract('Pressure Solver PV')[0].data)
-tot = cubes.extract('Total PV')[0].data
-p_theta = cubes.extract('air_pressure')[1].data
-adv500 = interpolate.any_to_pressure(adv,p_theta,[500])
-diab500 = interpolate.any_to_pressure(diab,p_theta,[500])
-other500 = interpolate.any_to_pressure(other,p_theta,[500])
-tot500 = interpolate.any_to_pressure(tot,p_theta,[500])
+cubes = files.load('/projects/diamet/lsaffi/xjjhq/*010.pp')
+adv = convert.calc('advection_only_pv', cubes)
+tot = convert.calc('total_pv', cubes)
+p = cubes.extract('air_pressure')[1]
 
-# Calculate the residual using PV tracers
-res1 = tot500 - adv500 - diab500 - other500
-# Calculate the residual from reverse domain filled PV
-res2 = tot500 - field - diab500 - other500
+# Add pressure as a coordinate
+pcoord = grid.make_coord(p)
+adv.add_aux_coord(pcoord, [0, 1, 2])
+tot.add_aux_coord(pcoord, [0, 1, 2])
 
-# Plot the residuals
-cscale = np.arange(-2,2 + 2/9.0,2/4.5)
-plt.figure(1)
-plt.contourf(lon,lat,res1[0,:,:],cscale,cmap='bwr',extend='both')
-plt.colorbar()
-#plt.contour(lon,lat,lsm,colors='k')
-plt.title('IOP5 T+35H PV Budget Residual at 500hPa')
-plt.savefig('residual_normal.png')
-plt.figure(2)
-plt.contourf(lon,lat,res2[0,:,:],cscale,cmap='bwr',extend='both')
-plt.colorbar()
-#plt.contour(lon,lat,lsm,colors='k')
-plt.title('IOP5 T+35H PV Budget Residual at 500hPa')
-plt.savefig('residual_trajectory.png')
+# Interpolate to pressure levels
+adv = interpolate.main(adv, air_pressure=500)
+tot = interpolate.main(tot, air_pressure=500)
+
+rdf_pv = rdf_pv.T[15:345, 15:585]
+rdf_pv = cubes[0][0].copy(data=rdf_pv)
+
+cscale = np.linspace(0, 5, 21)
+for cube in [adv, tot, rdf_pv]:
+    plt.figure()
+    plot.level(cube, cube)
+    plt.savefig('/home/lsaffi/plots/' + cube.name() + '.png')
