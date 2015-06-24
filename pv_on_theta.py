aed3e06d8366b0701eb2829c73b063ccadc348f6
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-06-24 12:04:51 +0000

    Working pv on theta routine

--- pv_on_theta.py
+++ pv_on_theta.py
@@ -1,38 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mymodule import io, convert, grid, interpolate, plot
+from mymodule import files, convert, grid, interpolate, plot
 
 
 def main(filename, name, theta_value):
-    cubes = io.load(filename)
+    cubes = files.load(filename)
     cubes.remove(cubes.extract('air_pressure')[0])
     x = convert.calc(name, cubes)
 
-    pv = convert.calc('total_pv', cubes)
+    pv = convert.calc('advection_only_pv', cubes)
     theta = convert.calc('air_potential_temperature', cubes)
     thcoord = grid.make_coord(theta)
     pv.add_aux_coord(thcoord, [0, 1, 2])
     x.add_aux_coord(thcoord, [0, 1, 2])
 
-    pv_on_theta = interpolate.to_level(pv, 'air_potential_temperature',
-                                       theta_value)
-    pv_on_theta = pv[0].copy(data=pv_on_theta)
+    pv_on_theta = interpolate.to_level(pv,
+                                       air_potential_temperature=[theta_value])
 
-    x_on_theta = interpolate.to_level(pv, 'air_potential_temperature',
-                                      theta_value)
-    x_on_theta = x[0].copy(data=x_on_theta)
+    x_on_theta = interpolate.to_level(x,
+                                      air_potential_temperature=[theta_value])
 
-    io.save(pv_on_theta, '/home/lsaffi/data/' + name + '_on_theta_' +
-            str(theta_value) + '.nc')
+    files.save([pv_on_theta, x_on_theta],
+               ('/home/lsaffi/data/' + name + '_on_theta_' +
+                str(theta_value) + '.nc'))
 
     levs = np.linspace(-2.1, 2.1, 16)
-    plot.level(x_on_theta, pv_on_theta, levs, cmap='cubehelix_r',
-               extend='both')
+    plot.level(x_on_theta[0], pv_on_theta[0], levs, cmap='bwr', extend='both')
     plt.savefig('/home/lsaffi/plots/' + name + '_on_theta.png')
 
 
 if __name__ == '__main__':
-    filename = '/projects/diamet/lsaffi/xjjhq/*030'
-    name = 'advection_inconsistency_pv'
-    theta_value = 330
+    filename = '/projects/diamet/lsaffi/xjjhq/*036.pp'
+    name = 'final_residual_pv'
+    theta_value = 320
     main(filename, name, theta_value)
