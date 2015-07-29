3af542635aa31db88248b5d6295d0c892128e216
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-07-29 10:21:27 +0000

    Add humidity threshold to dipole

--- dipole.py
+++ dipole.py
@@ -29,7 +29,7 @@ def load(filenames, varnames):
 
     # Extract relevant variables
     cubelist.remove(cubelist.extract('air_pressure')[0])
-    pv1 = convert.calc('total_pv', cubelist)
+    pv1 = convert.calc('advection_only_pv', cubelist)
     pv2 = convert.calc('advection_only_pv', cubelist)
     q = convert.calc('specific_humidity', cubelist)
 
@@ -50,7 +50,7 @@ def load(filenames, varnames):
 
 def calculate(pv1, pv2, q, mass, surf, variables):
     # Make a tropopause masked
-    trop = diagnostic.tropopause2(pv1, q)
+    trop = diagnostic.tropopause2(pv1, q, qmax=0.001)
 
     mask = surf.data * np.ones(pv1.shape) > pv1.coord('altitude').points
     mask = np.logical_or(np.logical_not(trop), mask)
@@ -64,11 +64,14 @@ def plotfig(means, masses, bins, varnames):
     bin_centres = 0.5 * (bins[0:-1] + bins[1:])
     for mean, name in zip(means, varnames):
         plot.dipole(bin_centres, mean, label=name)
+    plt.axis([0,8,-0.25,0.25])
     plt.legend(loc='best')
-    plt.savefig('/home/lsaffi/plots/IOP5/dipole/36h_paper.png')
+    plt.savefig('/home/lsaffi/plots/IOP5/dipole/36h_adv_dipole.png')
     plt.clf()
     plt.bar(bins[0:-1], masses, width=bins[1] - bins[0])
-    plt.savefig('/home/lsaffi/plots/IOP5/dipole/masses_1km.png')
+    plt.ylabel('Mass (kg)')
+    plt.xlabel('Advection Only PV (PVU)')
+    plt.savefig('/home/lsaffi/plots/IOP5/dipole/36h_mass.png')
 
 if __name__ == '__main__':
     binmin = 0.0
@@ -77,7 +80,7 @@ if __name__ == '__main__':
     nbins = int((binmax - binmin) / binspace) + 1
     bins = np.linspace(binmin, binmax, nbins)
 
-    filenames = '/projects/diamet/lsaffi/xjjhq/xjjha_036.pp'
+    filenames = '/projects/diamet/lsaffi/xjjhq/xjjhqa_036.pp'
 
     variables = ['total_minus_advection_only_pv',
                  'sum_of_physics_pv_tracers',
