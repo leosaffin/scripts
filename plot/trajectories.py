7d79052f95c7e2f699ed353601c734a48761e0dc
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-07-29 10:22:37 +0000

    Better calculation of rdf_pv

--- plot/trajectories.py
+++ plot/trajectories.py
@@ -1,34 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cPickle as pickle
+import iris
 from mymodule import files, convert, grid, interpolate, plot
 
 # Load RDF PV
-with open('/home/lsaffi/data/rdf_pv.pkl', 'rb') as infile:
-    rdf_pv = pickle.load(infile)
+rdf_pv = iris.load('/home/lsaffi/data/rdf_pv_trace.nc')[0]
+rdf_pv.rename('reverse_domain_filled_pv')
 
 # Load final time PV tracers
 cubes = files.load('/projects/diamet/lsaffi/xjjhq/*035.pp')
 adv = convert.calc('advection_only_pv', cubes)
 tot = convert.calc('total_pv', cubes)
+qsum = convert.calc('sum_of_physics_pv_tracers', cubes)
 p = cubes.extract('air_pressure')[1]
 
 # Add pressure as a coordinate
 pcoord = grid.make_coord(p)
 adv.add_aux_coord(pcoord, [0, 1, 2])
 tot.add_aux_coord(pcoord, [0, 1, 2])
+qsum.add_aux_coord(pcoord, [0, 1, 2])
 
 # Interpolate to pressure levels
-adv = interpolate.to_level(adv, air_pressure=[5])[0]
-tot = interpolate.to_level(tot, air_pressure=[5])[0]
+adv = interpolate.to_level(adv, air_pressure=[500])[0]
+tot = interpolate.to_level(tot, air_pressure=[500])[0]
+qsum = interpolate.to_level(qsum, air_pressure=[500])[0]
 
-rdf_pv = rdf_pv.T[15:345, 15:585]
-rdf_pv = cubes[0][0].copy(data=rdf_pv)
-rdf_pv.rename('reverse_domain_filled_pv')
-files.save([rdf_pv], '/home/lsaffi/data/IOP5/rdf_pv.pp')
+# Calculate epsilon from two different measure of conserved PV
+res1 = tot.data - adv.data - qsum.data
+res2 = tot.data - rdf_pv.data - qsum.data
 
-levs = np.linspace(0, 2, 11)
+# Make epsilons into iris cubes
+res1 = adv.copy(data=res1)
+res1.rename('epsilon')
+res2 = adv.copy(data=res2)
+res2.rename('epsilon_rdf')
+
+# Plot full PV measures
+levs = np.linspace(0, 2, 9)
 for cube in [adv, tot, rdf_pv]:
     plt.figure()
-    plot.level(cube, cube, levs, cmap='cubehelix_r', extend='both')
-    plt.savefig('/home/lsaffi/plots/' + cube.name() + '.png')
+    plot.level(cube, adv, levs, cmap='cubehelix_r', extend='both')
+    plt.savefig('/home/lsaffi/plots/' + cube.name() + '_500hpa.png')
+'''
+# Plot epsilons
+levs = np.linspace(-2.1, 2.1, 16)
+levs = [-2, -1.75, -1.5, -1.25, -1, -0.75, -0.5, -0.25,
+        0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+for cube in [res1, res2]:
+    plt.figure()
+    plot.level(cube, cube, levs, cmap='bwr', extend='both')
+    plt.savefig('/home/lsaffi/plots/' + cube.name() + '_500hpa.png')
+'''
--- rdf_pv.py
+++ rdf_pv.py
@@ -1,81 +1,127 @@
-from mymodule import forecast, interpolate, convert, grid, files, plot
-from mymodule.trajectory import load
-from iris.analysis.cartography import rotate_pole
+import cPickle as pickle
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
-import datetime
+from iris.analysis.cartography import rotate_pole, get_xy_grids
+from mymodule.trajectory import load as trload
+from mymodule import convert, grid, interpolate, files, plot
 
 
-name = 'advection_only_pv'
-
-
-def main(filename, start_time, mapping, pole_lon, pole_lat):
+def main(filename, forecast, name, pole_lon, pole_lat):
     # Load the trajectories
-    trajectories = load.raw(filename)
-    # Load the forecast data
-    forecast = forecast.Forecast(start_time, mapping)
+    trajectories = trload.raw(filename)
 
-    rlons = []
-    rlats = []
-    values = []
-    for trajectory in trajectories:
-        # Find the variable at the last trajectory point
-        time, lon, lat, p = trajectory.data[-1]
-        rlon, rlat = rotate_pole(np.array(lon), np.array(lat),
-                                 pole_lon, pole_lat)
-        forecast.set_time(time)
-        cube = convert.calc(name, forecast.cubelist)
+    # Calculate variable at the start of all trajectories
+    rlons, rlats, values, cube = rdf(name, trajectories, pole_lon, pole_lat)
 
-        # Add pressure as an Auxiliary coordinate
-        pressure = forecast.cubelist.extract('air_pressure')[1]
-        pcoord = grid.make_coord(pressure)
-        cube.add_aux_coord(pcoord)
-
-        # Interpolate to the trajectory point
-        column = interpolate.main(cube, grid_longitude=rlon,
-                                  grid_latitude=rlat)
-        value = interpolate.main(column, air_pressure=p)
-
-        # Save the values for plotting
-        rlons.append(rlon)
-        rlats.append(rlat)
-        values.append(value)
+    # Save the preliminary data
+    with open('/home/lsaffi/data/rdf_pv_trace.pkl', 'w') as output:
+        pickle.dump((rlons, rlats, values), output)
 
     # Put the values on the normal grid
-    lons = cube.coord('grid_longitude').points()
-    lats = cube.coord('grid_latitude').points()
+    lons, lats = get_xy_grids(cube)
     field = regular_grid(values, rlons, rlats, lons, lats)
-    cube = cube.copy(data=field)
+    cube = cube[0].copy(data=field)
     cube.rename('name')
 
     # Save the field
-    files.save(cube, '/home/lsaffi/data/rdf_pv.nc')
+    files.save([cube], '/home/lsaffi/data/rdf_pv_trace.nc')
+
     # Plot the field
-    levs = np.linspace(0, 5, 21)
-    plot.level(cube, cube, levs, cmap='cubehelix_r', extend='both')
+    levs = np.linspace(0, 5, 41)
+    plotfig(cube, cube, levs, cmap='cubehelix_r', extend='both')
+
+
+def rdf(name, trajectories, pole_lon, pole_lat):
+    """ Get the values of variable at the start of trajectories
+
+    Args:
+        name (str): Name of variable to calculate at the start of trajectories
+        trajectories (list): Length n list of trajectory objects
+        pole_lon (float): Rotated pole longitude
+        pole_lat (float): Rotated pole latitude
+
+    Returns:
+        rlons (list): Length n list of rotated longitude at end of trajectories
+        rlons (list): Length n list of rotated latitude at end of trajectories
+        values (list): Length n list of variable at start of trajectories
+    """
+    # Initialise output lists
+    rlons = []
+    rlats = []
+    values = []
+
+    n = 0
+    # Loop over all trajectories
+    for trajectory in trajectories:
+        n += 1
+        if n % 10000 == 0:
+            print n
+        # Extract the position at the last trajectory point
+        time = trajectory.variable('time')[-1]
+        lon = trajectory.variable('lon')[-1]
+        lat = trajectory.variable('lat')[-1]
+        p = trajectory.variable('p')[-1]
+        pv = trajectory.variable('PV')[-1]
+        rlon, rlat = rotate_pole(np.array(lon), np.array(lat),
+                                 pole_lon, pole_lat)
+
+        # Extract the data from the forecast
+        if n == 1:
+            forecast.set_time(time)
+            cube = convert.calc(name, forecast.cubelist)
+
+        # Add pressure as an Auxiliary coordinate
+        #try:
+        #    pressure = forecast.cubelist.extract('air_pressure')[1]
+        #    pcoord = grid.make_coord(pressure)
+        #    cube.add_aux_coord(pcoord, [0, 1, 2])
+        #except ValueError:
+        #    pass
+
+        # Interpolate to the trajectory point
+        #column = interpolate.main(cube, grid_longitude=rlon,
+        #                          grid_latitude=rlat)
+        #value = interpolate.to_level(column, air_pressure=[p]).data[0, 0, 0]
+
+        # Get the starting position
+        lon = trajectory.variable('lon')[0]
+        lat = trajectory.variable('lat')[0]
+        rlon, rlat = rotate_pole(np.array(lon), np.array(lat),
+                                 pole_lon, pole_lat)
+
+        # Save the values for plotting
+        rlons.append(float(rlon + 360))
+        rlats.append(float(rlat))
+        values.append(float(pv))
+
+    return rlons, rlats, values, cube
 
 
 def regular_grid(field, lons, lats, grid_lon, grid_lat):
-    """
+    """ Puts the data on a regular grid
     Args:
-        field (list):
-        lons (list):
-        lats (list):
-        grid_lon (numpy.ndarray): 1d array
-        grid_lat (numpy.ndarray): 1d array
+        field (list): Length n, data
+        lons (list): Length n, longitude points
+        lats (list): Length n, latitude points
+        grid_lon (numpy.ndarray): 2d array of longitude grid
+        grid_lat (numpy.ndarray): 2d array of latitude grid
     """
     points = [lons, lats]
     points = np.transpose(np.array(points))
     return griddata(points, field, (grid_lon, grid_lat), method='linear')
 
+
+def plotfig(cube, pv, *args, **kwargs):
+    plot.level(cube, pv, *args, **kwargs)
+    plt.savefig('/home/lsaffi/plots/rdf_pv.png')
+
+
 if __name__ == '__main__':
+    name = 'advection_only_pv'
     pole_lon = 177.5
     pole_lat = 37.5
-    filename = '/home/lsaffi/data/out_1h.1'
-    directory = '/projects/diamet/lsaffi/xjjhq/'
-    start_time = datetime.datetime(2011, 11, 28, 12)
-    dt = datetime.timedelta(hours=1)
-    mapping = {start_time + n * dt: directory + '*' +
-               str(int((n * dt).total_seconds() / 3600)).zfill(3) + '.pp'
-               for n in xrange(1, 37)}
-    main(filename, start_time, mapping, pole_lon, pole_lat)
+    filename = '/home/lsaffi/data/trace.1'
+    with open('/home/lsaffi/data/forecasts/iop5.pkl', 'r') as infile:
+        forecast = pickle.load(infile)
+    main(filename, forecast, name, pole_lon, pole_lat)
