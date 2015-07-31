00c58b0f63541acd9f3ee502ba524d9138bc51d1
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-07-31 14:28:36 +0000

    Fixed deleting of used diagnostic suites

--- season/analysis.py
+++ season/analysis.py
@@ -16,13 +16,12 @@ def main(start_time, end_time, dt):
             # Load the files from mass to postproc and into a forecast
             new_forecast = archive.download(time)
             suites.append(Suite(new_forecast, job_ids[time]))
-        for suite in suites:
-            update_time(suite, time)
-
         time += dt
+        for suite in suites:
+            update_time(suite, time, suites)
 
 
-def update_time(suite, time):
+def update_time(suite, time, suites):
     try:
         suite.set_time(time)
         suite.analyse()
@@ -30,11 +29,12 @@ def update_time(suite, time):
         # Remove the suite if it has exceeded the time
         suite.save()
         archive.clean_up(suite.forecast.start_time)
+        suites.remove(suite)
         del suite
 
 
 if __name__ == '__main__':
     start_time = datetime.datetime(2013, 11, 1)
-    end_time = datetime.datetime(2014, 2, 3)
+    end_time = datetime.datetime(2014, 2, 4)
     dt = datetime.timedelta(hours=6)
     main(start_time, end_time, dt)
