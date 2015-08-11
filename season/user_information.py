e336e897012e93331cacccf24b9a03e74f50e90b
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-08-11 15:10:41 +0000

    Rearranged order of user information to allow reserved_ids to be used alone

--- season/user_information.py
+++ season/user_information.py
@@ -5,21 +5,28 @@ import datetime
 from string import ascii_lowercase
 
 # The start/end cftime of the runs period and the increment between start dumps
-start_time = datetime.datetime(2013, 11, 1)
-end_time = datetime.datetime(2014, 2, 4)
+first_start_time = datetime.datetime(2013, 11, 1)
+last_start_time = datetime.datetime(2014, 1, 31)
 dt = datetime.timedelta(days=1)
-nt = int((end_time - start_time).total_seconds() / dt.total_seconds())
-start_times = [start_time + n * dt for n in xrange(nt + 1)]
+
+time = first_start_time
+start_times = []
+while time <= last_start_time:
+    start_times.append(time)
+    time += dt
+
+# A list of the lead times output for each forecast
 lead_times = [datetime.timedelta(hours=n) for n in xrange(0, 61, 6)]
 
 # A list of reserved ids for the job identifiers
 identifiers = ['xlib', 'xlic', 'xlid', 'xlie']
 reserved_ids = [identifier + letter for identifier in identifiers
                 for letter in ascii_lowercase]
+# Re order reserved IDs into time order
+reserved_ids = reserved_ids[66:96] + reserved_ids[0:62]
 
 # Dictionary mapping start cftime to job ID
-job_ids = {time: ID for time, ID in
-           zip(start_times, reserved_ids[66:96] + reserved_ids[0:66])}
+job_ids = {time: ID for time, ID in zip(start_times, reserved_ids)}
 
 # Letters used in forecast .pp files
 letters = ['a', 'b', 'c', 'd']
