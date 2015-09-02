a665c8c41aa7852f89a83d7ccce7d3c47d7545db
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-09-02 14:45:32 +0000

    Added script to calculate and plot simple averaged vertical profiles

---
+++ plot/__init__.py
@@ -0,0 +1,8 @@
+# List of line styles to loop through when plotting
+linestyles = ['-', '--', '-.', ':',
+              '-', '--', '-.', ':',
+              '-', '--', '-.', ':']
+
+colors = ['k', 'k', 'k', 'k',
+          'b', 'b', 'b', 'b',
+          'g', 'g', 'g', 'g']
---
+++ vertical_profile.py
@@ -0,0 +1,41 @@
+"""Calculate averaged vertical profiles of PV tracers
+"""
+
+import matplotlib.pyplot as plt
+import iris.plot as iplt
+from iris.analysis import VARIANCE
+from mymodule import files, convert
+from scripts.plot import linestyles, colors
+
+
+def main(filename, names):
+    """
+    """
+    cubes = files.load(filename)
+    variables = [convert.calc(name, cubes) for name in names]
+    means = [x.collapsed(['grid_latitude', 'grid_longitude'], VARIANCE)
+             for x in variables]
+    plotfig(means)
+
+
+def plotfig(means):
+    for n, mean in enumerate(means):
+        iplt.plot(mean, mean.coord('level_height'), linestyle=linestyles[n],
+                  color=colors[n], label=mean.name().replace('_', ' '))
+    plt.axis([0, 1, 0, 18000])
+    plt.axvline(color='k')
+    plt.savefig('/home/lsaffi/plots/36h_vertical_profile.png')
+    plt.legend(loc='best')
+    plt.savefig('/home/lsaffi/plots/36h_vertical_profile_legend.png')
+
+
+if __name__ == '__main__':
+    filename = '/projects/diamet/lsaffi/xjjhq/xjjhqa_036.pp'
+    names = ['total_minus_advection_only_pv',
+             'long_wave_radiation_pv',
+             'microphysics_pv',
+             'convection_pv',
+             'boundary_layer_pv',
+             'advection_inconsistency_pv',
+             'residual_pv']
+    main(filename, names)
