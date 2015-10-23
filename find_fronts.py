75173ce640732d7a2c62b0aeae6c6ea413226734
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-10-23 10:34:59 +0100

    moved routines for fronts to mymodule

--- find_fronts.py
+++ find_fronts.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 import iris.quickplot as qplt
 import iris.plot as iplt
 from mymodule import files, convert, grid, interpolate
-from scripts import fronts
+from mymodule.detection.fronts import fronts
 
 
-def main():
+def main(p_level):
     filename = '/home/lsaffin/Documents/meteorology/programming/iop5_36h.pp'
     cubes = files.load(filename)
     cubes.remove(cubes.extract('air_pressure')[0])
     theta = convert.calc('air_potential_temperature', cubes)
     p = grid.make_coord(cubes.extract('air_pressure')[0])
     theta.add_aux_coord(p, [0, 1, 2])
-    theta650 = interpolate.to_level(theta, air_pressure=[65000])
-    loc = fronts.main(theta650.data[0])
-    loc = theta650[0].copy(data=loc)
-    qplt.contourf(theta650[0], np.linspace(280, 320, 31), cmap='gray',
+    theta_p = interpolate.to_level(theta, air_pressure=[p_level])
+    loc = fronts.main(theta_p.data[0])
+    loc = theta_p[0].copy(data=loc)
+    qplt.contourf(theta_p[0], np.linspace(280, 320, 41), cmap='gray',
                   extend='both')
     plt.gca().coastlines()
     plt.gca().gridlines()
@@ -26,4 +26,5 @@ def main():
 
 
 if __name__ == '__main__':
-    loc = main()
+    p_level = 85000
+    main(p_level)
