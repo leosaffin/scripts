efc5eb4517eabcb9921c4a516cd70c16d77852ba
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-10-23 15:25:19 +0100

    Removed obsolete files

--- plot/__init__.py
+++
@@ -1,8 +0,0 @@
-# List of line styles to loop through when plotting
-linestyles = ['-', '--', '-.', ':',
-              '-', '--', '-.', ':',
-              '-', '--', '-.', ':']
-
-colors = ['k', 'k', 'k', 'k',
-          'b', 'b', 'b', 'b',
-          'g', 'g', 'g', 'g']
