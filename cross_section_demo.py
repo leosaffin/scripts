8246a0bd034ae999ce80752ff3268ca8f3b8cd02
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2015-12-04 11:38:35 +0000

    Reduced cross_section_demo to a minimal example

--- cross_section_demo.py
+++ cross_section_demo.py
@@ -1,43 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import RectangleSelector
-import iris
-import iris.plot as iplt
 import iris.quickplot as qplt
-from mymodule import files, convert, plot, interpolate, grid
-from mymodule.plot import interactive
+from mymodule import files, convert
+from mymodule.plot.interactive import CrossSection as CS
+from mymodule.plot.interactive import CrossSectionPlotter as CSP
 
 
 def main(cubes):
     """
     """
-    diff = convert.calc('total_minus_advection_only_pv', cubes)
     theta = convert.calc('air_potential_temperature', cubes)
-    pv = convert.calc('ertel_potential_vorticity', cubes)
-    p = convert.calc('air_pressure', cubes)
-    p = grid.make_coord(p)
-    theta.add_aux_coord(p, [0, 1, 2])
 
-    theta_p = interpolate.to_level(theta, air_pressure=[80000])
-    theta.remove_coord('air_pressure')
+    plotter = CSP(False, qplt.contourf, theta, np.linspace(270, 350, 15),
+                  cmap='cubehelix_r', extend='both')
 
-    plotter1 = interactive.CrossSectionPlotter(diff, qplt.contourf,
-                                               plot.even_cscale(2), cmap='bwr',
-                                               extend='both')
+    onselect = CS([plotter])
 
-    plotter2 = interactive.CrossSectionPlotter(theta, iplt.contour, [287],
-                                               colors='r')
-    plotter3 = interactive.CrossSectionPlotter(pv, plot.add_pv2)
-
-    onselect = interactive.CrossSection([plotter1, plotter2, plotter3])
-
-    qplt.contourf(theta_p[0], np.linspace(280, 320, 21), cmap='cubehelix_r')
-    widget = RectangleSelector(plt.gca(), onselect, drawtype='line')
+    qplt.contourf(theta[0], np.linspace(270, 300, 31), cmap='cubehelix_r')
+    RectangleSelector(plt.gca(), onselect, drawtype='line')
 
     plt.show()
 
 if __name__ == '__main__':
     filename = '/home/lsaffin/Documents/meteorology/programming/iop5_36h.pp'
-    cubes = files.load(filename,
-                       iris.Constraint(model_level_number=lambda cell: cell <= 30))
+    cubes = files.load(filename)
     main(cubes)
