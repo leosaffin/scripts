f87096d8dcd14bf399265a6faeba30573c6b40fb
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2016-01-13 12:50:34 +0000

    Created a case studies script to quickly create forecast objects

---
+++ case_studies.py
@@ -0,0 +1,25 @@
+"""Forecast objects for
+"""
+from datetime import datetime, timedelta
+from mymodule.forecast import Forecast
+
+
+def iop5():
+    job_name = 'xjjhq'
+    start_time = datetime(2011, 11, 28, 0)
+    mapping = {start_time + timedelta(hours=lead_time):
+               'datadir/' + job_name + '/' + job_name + 'a_' +
+               str(lead_time).zfill(3) + '.pp'
+               for lead_time in xrange(1, 37)}
+    return Forecast(start_time, mapping)
+
+
+def iop8():
+    job_name = 'xjjhq'
+    start_time = datetime(2011, 12, 7, 12)
+    mapping = {start_time + timedelta(hours=lead_time):
+               'datadir/' + job_name + '/' + job_name + '_p*' +
+               str(lead_time).zfill(3)
+               for lead_time in xrange(36)}
+
+    return Forecast(start_time, mapping)
