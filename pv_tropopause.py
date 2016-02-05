d1da06312a34f1a2647cc7b4086763195452f0c7
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-02-05 16:16:08 +0000

    Used forecast objects in PV tropopause

--- pv_tropopause.py
+++ pv_tropopause.py
@@ -1,23 +1,23 @@
+from datetime import timedelta as dt
 import numpy as np
 import matplotlib.pyplot as plt
-from mymodule import files, convert, grid, interpolate, plot
+from mymodule import convert, grid, interpolate, plot
+from scripts import case_studies
 
 
-def main(filename, dz, names, titles):
+def main(cubes, dz, name, title):
     """Produces cross section plots above and below the tropopause
     """
-    cubes = files.load(filename)
     pv = convert.calc('advection_only_pv', cubes)
     z = grid.make_cube(pv, 'altitude')
-    plotcubes = [convert.calc(name, cubes) for name in names]
+    cube = convert.calc(name, cubes)
 
     # Add PV as a coordinate to altitude
-    pv.rename('ertel_potential_vorticity')
     pv = grid.make_coord(pv)
     z.add_aux_coord(pv, [0, 1, 2])
 
     # Find the height of the tropopause
-    zpv2 = interpolate.to_level(z, ertel_potential_vorticity=[2])[0]
+    zpv2 = interpolate.to_level(z, advection_only_pv=[2])[0]
 
     # Produce a new co-ordinate above and below the tropopause
     ny, nx = zpv2.shape
@@ -28,23 +28,22 @@ def main(filename, dz, names, titles):
 
     # Interpolate the cubes to be plotted to the coordinate above and below the
     # tropopause
-    plotcubes = [interpolate.to_level(cube, altitude=new_coord)
-                 for cube in plotcubes]
+    plotcube = interpolate.to_level(cube, altitude=new_coord)
 
     # Plot the cross sections
     extension = [r' - 1$ km', r'$', r' + 1$ km']
-    for i, cube in enumerate(plotcubes):
-        for n in xrange(3):
-            plt.figure()
-            plot.contourf(cube[n], plot.even_cscale(2), cmap='bwr',
-                          extend='both')
-            plt.title(titles[i] + r' at $z(q_{adv}=2 PVU)' + extension[n])
+    for n in xrange(3):
+        plt.figure()
+        plot.pcolormesh(plotcube[n], vmin=-2, vmax=2, cmap='bwr')
+        plt.title(title + r' at $z(q_{adv}=2 PVU)' + extension[n])
     plt.show()
 
 
 if __name__ == '__main__':
-    filename = '../iop5_36h.pp'
+    forecast = case_studies.iop8()
+    forecast.set_lead_time(dt(hours=36))
+    cubes = forecast.cubelist
     dz = 1000
-    names = ['total_minus_advection_only_pv', 'sum_of_physics_pv_tracers']
-    titles = [r'$q-q_{adv}$', r'$\sum q_{phys}$']
-    main(filename, dz, names, titles)
+    name = 'sum_of_physics_pv_tracers'
+    title = r'IOP8: $\sum q_{phys}$'
+    main(cubes, dz, name, title)
