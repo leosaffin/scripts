cd4f1317376f6c452ffb714c89642f98d88c46f6
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-02-05 16:15:17 +0000

    Simplified PV on theta with new updates

--- pv_on_theta.py
+++ pv_on_theta.py
@@ -1,32 +1,31 @@
+from datetime import timedelta as dt
 import matplotlib.pyplot as plt
 import iris.plot as iplt
 from iris.time import PartialDateTime as PDT
-from mymodule import files, convert, grid, interpolate, plot
+from mymodule import convert, grid, plot
 from mymodule.detection import rossby_waves
+from scripts import case_studies
 
 
 def main(cubes, theta_value, **kwargs):
-    pv = convert.calc('ertel_potential_vorticity', cubes)
-    theta = convert.calc('air_potential_temperature', cubes)
-
-    pv.add_aux_coord(grid.make_coord(theta), [0, 1, 2])
-
-    # Interpolate pv to theta level
-    pv = interpolate.to_level(pv, air_potential_temperature=[theta_value])[0]
+    pv = convert.calc('ertel_potential_vorticity', cubes,
+                      levels=('air_potential_temperature', [theta_value]))[0]
 
     # Add equivalent latitude
-    lon, lat = grid.true_coords(theta)
+    lon, lat = grid.true_coords(pv)
     lat = pv.copy(data=lat)
-    time = grid.get_datetime(theta)[0]
+    time = grid.get_datetime(pv)[0]
     time = PDT(month=time.month, day=time.day, hour=time.hour)
     eqlat = rossby_waves.equivalent_latitude(time, theta_value, 2)
 
     # Plot PV on theta
     plot.pcolormesh(pv, pv=pv, **kwargs)
-    iplt.contour(lat, [eqlat.data], colors='r', linewidths=3)
+    iplt.contour(lat, [eqlat.data], colors='r', linewidths=2)
     plt.show()
 
 if __name__ == '__main__':
-    cubes = files.load('datadir/xjjhq/xjjhq_036.pp')
-    theta_value = 320
+    forecast = case_studies.iop8()
+    forecast.set_lead_time(dt(hours=36))
+    cubes = forecast.cubelist
+    theta_value = 310
     main(cubes, theta_value, vmin=0, vmax=10, cmap='plasma')
