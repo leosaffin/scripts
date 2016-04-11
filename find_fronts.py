425b6f2f29cab28186ef871fd5f79d26a9fbf3fb
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-04-11 14:58:15 +0100

    Simplified find fronts routine

--- find_fronts.py
+++ find_fronts.py
@@ -1,37 +1,29 @@
-import numpy as np
+from datetime import timedelta as dt
 import matplotlib.pyplot as plt
-import iris.quickplot as qplt
 import iris.plot as iplt
-from mymodule import files, convert, grid, interpolate
+from mymodule import convert, plot
 from mymodule.detection.fronts import fronts
+from scripts import case_studies
 
 
-def main(p_level):
+def main(cubes, p_level):
     # Load data
-    filename = '/home/lsaffin/Documents/meteorology/programming/iop5_36h.pp'
-    cubes = files.load(filename)
-    cubes.remove(cubes.extract('air_pressure')[0])
-    theta = convert.calc('air_potential_temperature', cubes)
-
-    # Interpolate to pressure level
-    p = grid.make_coord(cubes.extract('air_pressure')[0])
-    theta.add_aux_coord(p, [0, 1, 2])
-    theta_p = interpolate.to_level(theta, air_pressure=[p_level])[0]
+    theta = convert.calc('air_potential_temperature', cubes,
+                         levels=('air_pressure', [p_level]))[0]
 
     # Calculate the fronts
-    loc = fronts.main(theta_p)
-    loc = theta_p.copy(data=loc)
+    loc = fronts.main(theta)
+    loc = theta.copy(data=loc)
 
     # Plot the output
-    qplt.contourf(theta_p, np.linspace(280, 320, 41),
-                  cmap='gray', extend='both')
-    plt.gca().coastlines()
-    plt.gca().gridlines()
+    plot.pcolormesh(theta, vmin=280, vmax=320, cmap='plasma')
     plt.title(r'$\theta$ at ' + str(p_level) + ' Pa')
-    iplt.contour(loc, [0], colors='b', linewidths=3)
-    plt.savefig('../test_fronts.png')
+    iplt.contour(loc, [0], colors='k')
+    plt.show()
 
 
 if __name__ == '__main__':
+    forecast = case_studies.iop8()
+    cubes = forecast.set_lead_time(dt(hours=36))
     p_level = 65000
-    main(p_level)
+    main(cubes, p_level)
