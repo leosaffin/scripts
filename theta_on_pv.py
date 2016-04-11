00449c3da26cca014e7d013e6b2dc237e476b7cf
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-04-11 15:00:29 +0100

    Use forecast object for theta on PV

--- theta_on_pv.py
+++ theta_on_pv.py
@@ -1,16 +1,17 @@
+from datetime import timedelta as dt
 import matplotlib.pyplot as plt
-from mymodule import files, convert, plot
+from mymodule import convert, plot
+from scripts import case_studies
 
 
-def main(cubes):
-    """
-    """
+def main(cubes, **kwargs):
     theta = convert.calc('air_potential_temperature', cubes,
                          levels=('ertel_potential_vorticity', [2]))
 
-    plot.pcolormesh(theta[0], cmap='plasma')
+    plot.pcolormesh(theta[0], **kwargs)
     plt.show()
 
 if __name__ == '__main__':
-    cubes = files.load('datadir/xjjhq/xjjhq_036.pp')
-    main(cubes)
+    forecast = case_studies.iop5()
+    cubes = forecast.set_lead_time(dt(hours=36))
+    main(cubes, vmin=285, vmax=350, cmap='plasma')
