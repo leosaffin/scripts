3aecbfc3b34935f4f4bad461adee3f1ea7b25198
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-07-11 14:11:47 +0100

    Trajectory calculation return dataframe style object

--- trajectories/rdf_w.py
+++ trajectories/rdf_w.py
@@ -2,11 +2,14 @@
 co-ordinates.
 """
 
-from datetime import timedelta as dt
 import numpy as np
 from mymodule import grid, convert, interpolate
 from lagranto.caltra import caltra
 from scripts import case_studies
+
+tracers = ['ertel_potential_vorticity', 'advection_only_pv',
+           'air_pressure', 'air_potential_temperature',
+           'advection_only_potential_temperature']
 
 
 def main():
@@ -16,21 +19,42 @@ def main():
 
     # Create a set of start points for every grid point at 500hpa
     cubes = forecast.set_lead_time(hours=36)
-    P = convert.calc('air_pressure', cubes)
+    train = create_startpoints(cubes, 'air_pressure', [50000])
+
+    # Calculate the back trajectories
+    traout = caltra.caltra(train, mapping, fbflag=-1, tracers=tracers)
+
+    # Save the output
+    # traout.to_pickle('iop5_rdf_500hpa_36h.pkl')
+
+    return traout
+
+
+def create_startpoints(cubes, coordinate, values):
+    """Create an array of startpoints
+
+    Output array has shape Nx3 where N is the number of startpoints and the 3
+    indices are for longitude, latitude and altitude.
+
+    Args:
+        cubes (iris.cube.CubeList)
+
+    Returns:
+        train (np.array): The input array to trajectory calculations
+    """
+    # Get the values of altitude at the given coordinate
+    P = convert.calc(coordinate, cubes)
     z = grid.make_cube(P, 'altitude')
     z.add_aux_coord(grid.make_coord(P), [0, 1, 2])
-    z500 = interpolate.to_level(z, air_pressure=[50000])[0].data
+    z500 = interpolate.to_level(z, **{coordinate: values})[0].data
+
     lon, lat = grid.get_xy_grids(P)
+
+    # Convert the 2d arrays to an Nx3 array
     train = np.array(
         [lon.flatten(), lat.flatten(), z500.flatten()]).transpose()
 
-    # Calculate the back trajectories
-    traout = caltra.caltra(
-        train, mapping, fbflag= -1,
-        tracers=['ertel_potential_vorticity', 'advection_only_pv',
-                 'air_pressure', 'air_potential_temperature'])
-
-    np.save('/home/lsaffi/data/rdf_pv', traout)
+    return train
 
 if __name__ == '__main__':
     main()
