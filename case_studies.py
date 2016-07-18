75b31df481c54dee7219eaf20fe2c0da3a181cb2
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-07-18 17:03:31 +0100

    Added iop5 case study with monotone advection

--- case_studies.py
+++ case_studies.py
@@ -4,37 +4,62 @@ from datetime import datetime, timedelta
 from mymodule.forecast import Forecast
 
 
-def iop5():
-    job_name = 'xjjhq'
-    start_time = datetime(2011, 11, 28, 12)
-    mapping = {start_time + timedelta(hours=lead_time):
-               'datadir/' + job_name + '/' + job_name + 'a_' +
-               str(lead_time).zfill(3) + '.pp'
-               for lead_time in xrange(1, 37)}
-    return Forecast(start_time, mapping)
-
-
-def iop5b():
-    job_name = 'xjjhq'
-    start_time = datetime(2011, 11, 28, 12)
+def generate_forecast(start_time, lead_times, job_id, filenames, suffix='.nc'):
     mapping = {}
-    for lead_time in xrange(1, 37):
-        tracers_file = ('datadir/' + job_name + '/' + job_name + 'b_' +
-                        str(lead_time).zfill(3) + '.pp')
-        prognostics_file = ('datadir/' + job_name + '/' + job_name +
-                            'b_nddiag_' + str(lead_time).zfill(3) + '.pp')
-        mapping[start_time + timedelta(hours=lead_time)] = [tracers_file,
-                                                            prognostics_file]
+    for n, dt in enumerate(lead_times, start=1):
+        mapping[start_time + dt] = make_filenames(job_id, filenames, n, suffix)
 
     return Forecast(start_time, mapping)
+
+
+def make_filenames(job_id, filenames, n, suffix):
+    names = []
+    for filename in filenames:
+        names.append('datadir/' + job_id + '/' +
+                     filename + str(n).zfill(3) + suffix)
+
+    return names
+
+
+# Original IOP5 forecast with dynamics-tracer inconsistency including pressure
+# solver increments
+def iop5():
+    start_time = datetime(2011, 11, 28, 12)
+    lead_times = [timedelta(hours=n) for n in range(1, 37)]
+    job_id = 'xjjhq'
+    filenames = ['xjjhqa_']
+
+    return generate_forecast(start_time, lead_times, job_id,
+                             filenames, suffix='.pp')
+
+
+# Dynamics-tracer inconsistency using basic diagnostic without pressure solver
+# increments
+def iop5b():
+    start_time = datetime(2011, 11, 28, 12)
+    lead_times = [timedelta(hours=n) for n in range(1, 37)]
+    job_id = 'xjjhq'
+    filenames = ['xjjhqb_', 'xjjhqb_nddiag_']
+
+    return generate_forecast(start_time, lead_times, job_id,
+                             filenames, suffix='.pp')
+
+
+# Same as iop5b but using monotone limiter for PV tracer advection
+def iop5_mono():
+    start_time = datetime(2011, 11, 28, 12)
+    lead_times = [timedelta(hours=n) for n in range(1, 37)]
+    job_id = 'iop5'
+    filenames = ['prognostics_', 'diagnostics_', 'pv_tracer_mono_']
+
+    return generate_forecast(start_time, lead_times, job_id, filenames)
 
 
 def iop8():
-    job_name = 'xkcqa'
     start_time = datetime(2011, 12, 7, 12)
-    mapping = {start_time + timedelta(hours=lead_time):
-               'datadir/' + job_name + '/' + job_name + 'a*' +
-               str(lead_time).zfill(3) + '.pp'
-               for lead_time in xrange(1, 37)}
+    lead_times = [timedelta(hours=n) for n in range(1, 37)]
+    job_id = 'xkcqa'
+    filenames = ['xkcqaa_*']
 
-    return Forecast(start_time, mapping)
+    return generate_forecast(start_time, lead_times, job_id,
+                             filenames, suffix='.pp')
