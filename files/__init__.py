0dd0ed58ea888c591822926c629910dd1f95d4f2
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2016-09-13 13:27:01 +0000

    Keep altitude in forecast and analysis files

--- files/__init__.py
+++ files/__init__.py
@@ -8,7 +8,8 @@ def main():
     # Filename parameters
     strlen = 3
     inpath = '/projects/diamet/lsaffi/xjjhq/xjjhqa_p'
-    outpath = '/projects/diamet/lsaffi/iop5/'
+    outpath = '/projects/diamet/lsaffi/temp/'
+    time = 'hours since 2011-11-28 12:00:00'
 
     # Load grid orography
     z_0 = iris.load('/projects/diamet/lsaffi/nae_orography.nc')[0][0, 0]
@@ -23,20 +24,19 @@ def main():
         infile = inpath + 'a' + str(n).zfill(strlen)
         outfile = outpath + 'pv_tracers_' + str(n + 1).zfill(strlen)
         tracers(infile, outfile, stash_maps=[stash_maps.pv_tracers],
-                orography=orog, slices=slices)
+                orography=orog, slices=slices, time=time)
 
         # Prognostics
         nddiag_name = inpath + 'b' + str(n).zfill(strlen)
         progs_name = inpath + 'c' + str(n).zfill(strlen)
         outfile = outpath + 'prognostics_' + str(n + 1).zfill(strlen)
         prognostics(nddiag_name, progs_name, outfile, orography=orog,
-                    slices=slices)
+                    slices=slices, time=time)
 
         # Diagnostics
         infile = inpath + 'd' + str(n).zfill(strlen)
         outfile = outpath + 'diagnostics_' + str(n + 1).zfill(strlen)
-        diagnostics(infile, outfile,
-                    slices=slices[1:])
+        diagnostics(infile, outfile, slices=slices[1:], time=time)
 
 
 def ff2nc(infile, outfile, **kwargs):
@@ -83,7 +83,7 @@ def diagnostics(infile, outfile, **kwargs):
     iris.save(cubes, outfile + '.nc')
 
 
-def redo_cubes(cubes, stash_maps=[], orography=None, slices=None):
+def redo_cubes(cubes, stash_maps=[], orography=None, slices=None, time=None):
     # Define attributes of custom variables by stash mapping
     for stash_map in stash_maps:
         stash_map.remap_cubelist(cubes)
@@ -92,19 +92,27 @@ def redo_cubes(cubes, stash_maps=[], orography=None, slices=None):
         # Remove unneeded attributes
         cube.attributes = {}
 
-        # Remove all aux factories
-        for factory in cube.aux_factories:
-            cube.remove_aux_factory(factory)
-
         try:
             # Use the hybrid height coordinate as the main vertical coordinate
             cube.remove_coord('model_level_number')
             iris.util.promote_aux_coord_to_dim_coord(cube, 'level_height')
+
+            # Add hybrid height coordinates
+            if orography is not None:
+                cube.add_aux_coord(orography, [1, 2])
+                grid.add_hybrid_height(cube)
+
         except iris.exceptions.CoordinateNotFoundError:
+            # Coordinate not found for 2d cubes
             pass
 
-        if orography is not None:
-            cube.add_aux_coord(orography, [1, 2])
+        # Remove unneccessary time coordinates
+        for coord in ['forecast_period', 'forecast_reference_time']:
+            cube.remove_coord(coord)
+
+        # Convert the main time coordinate
+        if time is not None:
+            cube.coord('time').convert_units(time)
 
     return CubeList([cube[slices] for cube in cubes])
 
@@ -115,14 +123,13 @@ def _nddiag(newcubes, filename):
 
     # Extract w to use grid dimensions
     w = convert.calc('upward_air_velocity', cubes)
+    newcubes.append(w)
 
     # u and v are staggered on rho-levels
     for name in ('x_wind', 'y_wind'):
         cube = convert.calc(name, cubes)
         cube = interpolate.remap_3d(cube, w, 'level_height')
         newcubes.append(cube)
-
-    newcubes.append(w)
 
     # Simply copy across cubes on theta levels
     for name in ('specific_humidity',
--- files/analyses.py
+++ files/analyses.py
@@ -1,34 +1,40 @@
 """
-Start by running the script conv2nc.tcl on the raw analyses
+Start by running the script conv2nc.tcl on the raw analyses from the xcm
 """
 
 import iris
 from iris.cube import CubeList
 from iris.util import squeeze
-from mymodule import interpolate
+from mymodule import convert, interpolate
 
+# Define which area of grid to subset
+slices = slice(0, 50), slice(15, 345), slice(15, 585)
+
+# Define how to rename variables
 name_pairs = [('DENSITY*R*R AFTER TIMESTEP', 'air_density'),
               ('eastward_wind', 'x_wind'),
               ('northward_wind', 'y_wind')]
 
-units = [('air_density', 'kg m-3'),
+# Correct units for some cubes
+units = [('air_density', 'kg m-3'), q
          ('dimensionless_exner_function', '1'),
          ('mass_fraction_of_cloud_ice_in_air', '1'),
          ('mass_fraction_of_cloud_liquid_water_in_air', '1')]
 
-file_pairs = [('', ''),
-              ('', '')]
+path = '/projects/diamet/lsaffi/iop5/'
+file_pairs = [(path + '20111129_qwqy12.nc', path + 'analysis_024.nc')]
 
 
 def main():
-    path = '/projects/diamet/lsaffi/iop8/'
-
-    basis_cube = iris.load_cube(path + 'prognostics_001.nc')
+    basis_cube = iris.load_cube(path + 'prognostics_001.nc',
+                                'air_potential_temperature')
+    basis_cube.remove_coord('forecast_reference_time')
+    basis_cube.remove_coord('forecast_period')
 
     for infile, outfile in file_pairs:
-        cubes = iris.load(path + infile)
+        cubes = iris.load(infile)
         newcubes = redo_cubes(cubes, basis_cube)
-        iris.save(newcubes, path + outfile)
+        iris.save(newcubes, outfile)
 
 
 def redo_cubes(cubes, basis_cube):
@@ -38,28 +44,32 @@ def redo_cubes(cubes, basis_cube):
     lon = basis_cube.coord('grid_longitude')
     sigma = basis_cube.coord('sigma')
     surf = basis_cube.coord('surface_altitude')
+    altitude = basis_cube.coord('altitude')
 
     newcubelist = CubeList()
     for cube in cubes:
         # Squeeze cubes dimensions
         newcube = squeeze(cube)
+        newcube = newcube[slices]
 
         # Give time coordinate proper name
         newcube.coord('t').rename('time')
 
         # Remap the cubes to theta points
-        newcube.coord('Hybrid height').rename('level_height')
+        newcube.coord('Hybrid height').rename(z.name())
         newcube.coord('grid_latitude').coord_system = lat.coord_system
         newcube.coord('grid_longitude').coord_system = lon.coord_system
         newcube = interpolate.remap_3d(newcube, basis_cube, 'level_height')
 
         # Give the cube the standard coordinates
-        for coord in (z, lat, lon):
-            newcube.replace_coord(coord)
+        for n, coord in enumerate([z, lat, lon]):
+            newcube.remove_coord(coord.name())
+            newcube.add_dim_coord(coord, n)
 
         # Add extra auxiliary coordinates
         newcube.add_aux_coord(sigma, 0)
         newcube.add_aux_coord(surf, [1, 2])
+        newcube.add_aux_coord(altitude, [0, 1, 2])
 
         newcubelist.append(newcube)
 
@@ -67,8 +77,17 @@ def redo_cubes(cubes, basis_cube):
     for before, after in name_pairs:
         newcubelist.extract(before)[0].rename(after)
 
+    # Convert density to true density
+    rho = convert.calc('air_density', cubes)
+    z = rho.coord('altitude')
+    r = z.data + convert.r.data
+    rho.data = rho.data / r ** 2
+
     # Correct units
     for name, unit in units:
         newcubelist.extract(name)[0].units = unit
 
     return newcubelist
+
+if __name__ == '__main__':
+    main()
