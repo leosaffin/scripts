ef0ed3f60a1076fba92cbb62abe140729e99a1f0
lsaffin <l.saffin@pgr.reading.ac.uk>
2017-07-07 11:04:07 +0100

    Add directory for plots in dynamics PV paper

---
+++ papers/dynamics_pv/pv_budget.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import iris.plot as iplt
+from mymodule import convert, plot
+from scripts import case_studies
+
+
+levels = ('air_potential_temperature', [320])
+clevs = plot.even_cscale(2)
+cmap = 'coolwarm'
+
+
+def main(cubes):
+    pv = convert.calc('ertel_potential_vorticity', cubes, levels=levels)[0]
+    adv = convert.calc('advection_only_pv', cubes, levels=levels)[0]
+    for n, name in enumerate(['sum_of_physics_pv_tracers', 'epsilon',
+                              'dynamics_tracer_inconsistency', 'residual_pv']):
+        cube = convert.calc(name, cubes, levels=levels)[0]
+
+        m = n / 2
+        ax = plt.subplot2grid((2, 2), (n - 2 * m, m))
+        iplt.contourf(cube, clevs, cmap=cmap)
+        plot._add_map()
+        iplt.contour(pv, [2], colors='k', linestyles='-')
+        iplt.contour(adv, [2], colors='k', linestyles='-')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    forecast = case_studies.iop5b.copy()
+    cubes = forecast.set_lead_time(hours=24)
+    main(cubes)
