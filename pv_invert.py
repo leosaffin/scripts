3e60e146de8a74d5e00aeda003ea092633e61c96
lsaffin <l.saffin@pgr.reading.ac.uk>
2017-07-07 11:02:56 +0100

    Tidied up PV inversion wrapping code

--- pv_invert.py
+++ pv_invert.py
@@ -1,59 +1,61 @@
 import numpy as np
-from numpy import pi
-import grid
-import interpolate
-import convert
+from mymodule import grid, variable
 import pvi
 
-omega = 7.2921e-5 # Coriolis Term
-gravity = 9.81    # Gravitational Acceleration
-f_0 = 1.e-4       # Typical Coriolis Parameter
-ll = 12000        # dy in m
-tho = f_0*f_0*ll*ll
-laplacian_coefficients = np.zeros((ny,5))
-for i in xrange(0,ny):
-    laplacian_coefficients[i,0] = 1.0
-    laplacian_coefficients[i,1] = 1.0
-    laplacian_coefficients[i,2] = -4.0
-    laplacian_coefficients[i,3] = 1.0
-    laplacian_coefficients[i,4] = 1.0
+omega = 7.2921e-5  # Coriolis Term
+gravity = 9.81     # Gravitational Acceleration
+f_0 = 1.e-4        # Typical Coriolis Parameter
+ll = 12000         # dy in m
+tho = f_0 * f_0 * ll * ll
 
-def pv_invert(pv, boundary_theta, levels=np.arange(50,1000,50), 
-              relaxation_parameter=0.4, max_iterations=1999, max_cycles=999,
-              omega_s=1.7, omega_h=1.4, thrs1 = 0.1, **kwargs):
 
-    threshold = gravity*thrs1/tho
-    [nz,ny,nx] = pv.data.shape
-    if 'polelon' in kwargs and 'polelat' in kwargs:
-        [lon,lat] = grid.unrotate(pv.coord('grid_longitude').points,
-                                  pv.coord('grid_latitude').points,
-                                  kwargs['polelon'],kwargs['polelat'])
-    else:
-        lon = pv.coords('grid_longitude').points()
-        lat = pv.coords('grid_latitude').points()
-        lon,lat = np.meshgrid(lon,lat)
+def pv_invert(pv, boundary_theta, thrs1=0.1, relaxation_parameter=0.4,
+              max_iterations=1999, max_cycles=999, omega_s=1.7, omega_h=1.4):
+    """Inverts pv to find balanced geopotential and streamfunction
 
-    coriolis_parameter = 2*omega*np.sin(lat)
+    Args:
+        pv (iris.cube.Cube):
+
+        boundary_theta (iris.cube.Cube):
+    """
+
+    threshold = gravity * thrs1 / tho
+
+    lon, lat = grid.true_coords(pv)
+
+    laplacian_coefficients = coefficients(len(lat))
+    coriolis_parameter = 2 * omega * np.sin(lat)
     cos_latitude = np.cos(lat)
-    [geopotential_height, streamfunction] = first_guess(pv)
-    exner = convert.pressure_to_exner(levels)
-    pv_in = pv.data 
+    geopotential_height, streamfunction = first_guess(pv)
 
-    [geopotential_height,
-     streamfunction,
-     pv_out,
-     boundary_theta]      = pvi.balnc(coriolis_parameter, cos_latitude,
-                                      laplacian_coefficients, 
-                                      geopotential_height,
-                                      streamfunction, pv_in, boundary_theta, 
-                                      exner, relaxation_parameter, threshold, 
-                                      max_iterations, max_cycles,
-                                      omega_s, omega_h)
+    # Extract the vertical coordinate from the cube
+    pressure = grid.make_cube(pv, 'air_pressure')
+    exner = variable.exner(pressure)
 
-    return [geopotential_height,streamfunction]
+    # Perform the PV inversion
+    geopotential_height, streamfunction, pv_out, boundary_theta = \
+        pvi.balnc(coriolis_parameter, cos_latitude, laplacian_coefficients,
+                  geopotential_height, streamfunction, pv.data,
+                  boundary_theta.data, exner, relaxation_parameter, threshold,
+                  max_iterations, max_cycles, omega_s, omega_h)
 
-# Gives a first guess for streamfunction and geopotential height
+    return pv_out, geopotential_height, streamfunction
+
+
+def coefficients(N):
+    laplacian_coefficients = np.zeros([N, 5])
+    laplacian_coefficients[:, 0] = 1.0
+    laplacian_coefficients[:, 1] = 1.0
+    laplacian_coefficients[:, 2] = -4.0
+    laplacian_coefficients[:, 3] = 1.0
+    laplacian_coefficients[:, 4] = 1.0
+
+    return laplacian_coefficients
+
+
 def first_guess(pv):
-    geopotential_height = np.ones(((pv.data.shape)))
+    """Gives a first guess for streamfunction and geopotential height
+    """
+    geopotential_height = np.ones_like(pv.data)
     streamfunction = geopotential_height
-    return [geopotential_height, streamfunction]
+    return geopotential_height, streamfunction
