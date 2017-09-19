0e723ac8c98cbec449254a57ee4a620c23e32a3e
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2017-09-19 13:52:27 +0000

    Used datadir in previously added script

---
+++ trajectories/select.py
@@ -0,0 +1,36 @@
+import datetime
+from mymodule import datadir
+from lagranto import trajectory
+
+def main():
+    job = 'iop5_extended'
+    name = 'forward_trajectories_from_low_levels'
+    filename = datadir + job + '/' + name + '.pkl'
+    
+    trajectories = select_wcb(filename)
+    trajectories.save(datadir + job + '/' + name + '_gt600hpa.pkl')
+
+    return
+
+
+def select_wcb(filename):
+    # Load the trajectories
+    trajectories = trajectory.load(filename)
+    print len(trajectories)
+
+    # Select trajectories that stay in the domain
+    trajectories = trajectories.select('air_pressure', '>', 0)
+    print len(trajectories)
+
+    # Select trajectories that fulfill the ascent criteria
+    dt1 = datetime.timedelta(hours=0)
+    dt2 = datetime.timedelta(hours=48)
+    trajectories = trajectories.select('air_pressure', '>', 60000,
+                                       time=[dt1, dt2])
+    print len(trajectories)
+
+    return trajectories
+
+
+if __name__ == '__main__':
+    main()
