bc486d5c064168657ee932126fb470859ab15c6b
Leo Saffin <l.saffin@pgr.reading.ac.uk>
2017-09-20 10:21:13 +0000

    Updated clustering methods

--- trajectories/cluster.py
+++ trajectories/cluster.py
@@ -1,21 +1,41 @@
-from datetime import timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, fcluster, linkage
 from scipy.cluster.vq import kmeans, vq
+from iris.analysis.cartography import rotate_pole
 from lagranto import trajectory
-from scripts import case_studies
+from mymodule.user_variables import datadir, plotdir
 
-path = '/home/lsaffin/Documents/meteorology/data/iop5/'
-name = 'backward_trajectories_from_320K'
-forecast = case_studies.iop5b.copy()
-cubes = forecast.set_lead_time(hours=24)
+styles = ['-bo', '-gx', '-rx', '-cx', '-mx', '-yx',
+          '-bx', '-go', '-ro', '-co', '-mo', '-yo']
+
+def main():
+    job = 'iop5_extended'
+    name = 'forward_trajectories_from_low_levels_gt600hpa'
+    trajectories = trajectory.load(datadir + job + '/' + name + '.pkl')
+    trajectories = trajectories.select('air_pressure', '>', 0)
+    
+    # Perform the clustering
+    cluster_array = make_cluster_array(trajectories)
+    clusters = perform_clustering(cluster_array)
+    plt.savefig(plotdir + job + '_' + name + '_cluster_info.png')
+    #np.save(datadir + job + '/' + name + '_clusters.npy', clusters)
+    #clusters = np.load(datadir + job + '/' + name + '_clusters.npy')
+    
+    # Display output
+    nclusters = len(set(clusters))
+    print('Number of clusters', nclusters)
+    plot_clusters(trajectories, clusters)
+    plt.savefig(plotdir + job + '_' + name + '_clusters_xy.png')
+    plt.show()
+
+    return
 
 
-def main(trajectories):
+def make_cluster_array(trajectories):
     # Normalise the trajectory positions
-    x = normalise(trajectories['grid_longitude'])
-    y = normalise(trajectories['grid_latitude'])
+    x = normalise(trajectories.x)
+    y = normalise(trajectories.y)
     z = normalise(trajectories['altitude'])
 
     # Rearrange array to 2d
@@ -26,51 +46,57 @@ def main(trajectories):
     for n in range(ntim):
         for m, obs in enumerate(observations):
             cluster_array[:, n + m * ntim] = obs[:, n]
+            
+    return cluster_array
 
-    # Perform clustering
-    clusters = hierarchical(cluster_array, max_dist=7)
-    #clusters = kmeans_cluster(cluster_array, 4)
 
-    nclusters = (len(set(clusters)))
-    print('Number of clusters', nclusters)
+def perform_clustering(cluster_array, method='average', max_dist=None,
+                       nclusters=2):
+    if method is 'kmeans':
+        clusters = kmeans_cluster(cluster_array, nclusters)
+    else:
+        clusters = hierarchical(cluster_array, method=method,
+                                max_dist=max_dist)
 
+    return clusters
+
+
+def plot_clusters(trajectories, clusters):
     plt.figure()
-    styles = ['bx', 'gx', 'rx', 'cx', 'mx']
-    xdata = trajectories['grid_longitude']
-    ydata = trajectories['grid_latitude']
-    # Plot each cluster
+    xdata = trajectories.x
+    ydata = trajectories.y
+
+    # Plot each trajectory coloured by cluster
     for n, i in enumerate(clusters):
-        plt.plot(xdata[n], ydata[n], '-k')
-        for j in [0, -1]:
-            plt.plot(xdata[n, j], ydata[n, j], styles[i % 5], zorder=5)
+        plt.plot(xdata[n], ydata[n], styles[i % 12], zorder=5)
 
     # Plot the cluster averages
     for n in set(clusters):
-        plt.plot(xdata[clusters == n].mean(axis=0),
-                 ydata[clusters == n].mean(axis=0),
-                 '-' + styles[n % 5], markersize=5, linewidth=2, zorder=6)
-    plt.show()
+        xm = np.median(xdata[clusters == n], axis=0)
+        ym = np.median(ydata[clusters == n], axis=0)
+        plt.plot(xm, ym, '-k', zorder=7)
 
     return
 
 
-def hierarchical(cluster_array, max_dist=None):
-    cluster_info = linkage(cluster_array, 'ward')
+def hierarchical(cluster_array, method='ward', max_dist=None):
+    cluster_info = linkage(cluster_array, method)
+    
+    difference = np.diff(cluster_info[:, 2], 2)
+    if max_dist is None:
+        max_dist = 1.05*cluster_info[difference.argmax(), 2]
 
     # Show dendrogram and distance
-    dendrogram(cluster_info)
+    plt.figure(figsize=[12, 15])
+    plt.subplot2grid([2, 1], [0, 0])
+    dendrogram(cluster_info, color_threshold=max_dist)
+    plt.axhline(max_dist, color='k', linestyle='--')
 
-    plt.figure()
+    plt.subplot2grid([2, 1], [1, 0])
     plt.plot(cluster_info[1:, 2], '-k')
-
     ax = plt.twinx()
-    difference = np.diff(cluster_info[:, 2], 2)
-    ax.plot(difference, '-r')
-
+    ax.plot(difference, '-rx')
     plt.xlim(len(cluster_info) - 22, len(cluster_info) - 2)
-
-    if max_dist is None:
-        max_dist = cluster_info[difference.argmax(), 2]
 
     # Select the clusters at a specified cut off
     clusters = fcluster(cluster_info, max_dist, criterion='distance')
@@ -96,12 +122,4 @@ def normalise(x):
 
 
 if __name__ == '__main__':
-    filename = path + name + '.pkl'
-    trajectories = trajectory.load(filename)
-    dt1 = timedelta(hours=0)
-    trajectories = trajectories.select('altitude', '>', 0)
-    trajectories = trajectories.select('grid_latitude', '>', 0, time=[dt1])
-    trajectories = trajectories.select('grid_longitude', '>', 360, time=[dt1])
-    trajectories = trajectories.select('grid_longitude', '<', 380, time=[dt1])
-    trajectories = trajectories.select('grid_latitude', '<', 10, time=[dt1])
-    main(trajectories)
+    main()
