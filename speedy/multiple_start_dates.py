f627068a8417fd3fcf8ead3dab97ce82a94ec96f
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2018-01-15 15:05:08 +0000

    Added script for assessing precision over many start dates

---
+++ speedy/multiple_start_dates.py
@@ -0,0 +1,57 @@
+"""
+Compare a set of runs with reduced precision to a reference double precision
+simulation. Each forecast uses the same random-number seed in the SPPT forcing
+so all errors are 'deterministic'.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import iris
+from iris.time import PartialDateTime
+from iris.analysis import STD_DEV, MEAN
+from iris.analysis.cartography import cosine_latitude_weights
+from mymodule import plot
+from scripts.speedy import plotdir
+from scripts.speedy import rms_diff
+
+datadir = '/home/saffin/cirrus/speedy/'
+
+
+def main():
+    # Parameters to compare between forecasts
+    name = 'Temperature [K]'
+    pressure = 500
+    cs = iris.Constraint(
+        name=name, pressure=pressure, forecast_reference_time=(
+            lambda cell: (PartialDateTime(year=1982) < cell <
+                          PartialDateTime(year=1992))))
+
+    # Load full precision reference forecast
+    with iris.FUTURE.context(cell_datetime_objects=True):
+        fp = iris.load_cube(datadir + '1982-1991_p52.nc', cs)
+
+        # Compare with reduced precision forecasts
+        rp = iris.load(datadir + '*_10-30.nc', cs)
+        rp = rp.concatenate_cube()
+
+    weights = cosine_latitude_weights(rp)
+    diff = rms_diff(rp, fp, weights)
+    mean = diff.collapsed('forecast_reference_time', MEAN)
+    std_dev = diff.collapsed('forecast_reference_time', STD_DEV)
+    std_dev = (std_dev /
+               np.sqrt(len(diff.coord('forecast_reference_time').points)))
+    
+    for mean_slice, std_slice in zip(mean.slices(['precision']),
+                                     std_dev.slices(['precision'])):
+        plot.errorbar(mean_slice, yerr=std_slice, marker='x')
+
+    plt.savefig(plotdir + 'average_errors' + '_' + name.split()[0].lower() +
+                '_' + str(pressure) + 'hpa.png')
+    plt.show()
+
+    return
+
+
+if __name__ == '__main__':
+    print('Executing ' + __name__ + ' as script.')
+    main()
