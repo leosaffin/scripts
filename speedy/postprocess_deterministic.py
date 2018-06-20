bcdf4a4b74ac684bd08f18cb6cd2f3ba559be063
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2018-06-20 16:35:04 +0100

    Split call to main from work done in postprocess_deterministic

--- speedy/postprocess_deterministic.py
+++ speedy/postprocess_deterministic.py
@@ -2,26 +2,56 @@ import datetime as dt
 import pandas as pd
 import iris
 from iris.coords import AuxCoord, DimCoord
-from iris.unit import Unit
+from cf_units import Unit
 from scripts.speedy import datadir
-
-datadir = datadir + 'exp_000/'
-#datadir = '/home/saffin/cirrus/speedy/'
 
 
 def main():
+    path = '/home/lsaffin/temp/'
+    prefix = 'tendencies'
     t0 = dt.datetime(1982, 1, 1)
     t1 = dt.datetime(1982, 1, 1)
-    pmin, pmax = 5, 52
+    pmin, pmax = 5, 23
+    nt = 32
+
+    cubes = gather_data(path, prefix, t0, t1, pmin, pmax, nt)
+    print(cubes)
+    path = datadir
+    iris.save(cubes, path + str(t0.year) + '-' + str(t1.year) + '_' +
+              str(pmin) + '-' + str(pmax) + '.nc')
+
+    return
+
+
+def gather_data(path, prefix, t0, t1, pmin, pmax, nt):
+    """
+    Filenames follow the structure '{prefix}_{precision}.nc' where prefix is a
+    generic prefix or the date in the format '{YYYYMMDDHH}'.
+
+    Args:
+        path (str): Directory containing forecast output
+        prefix (str): Start of filename
+        t0 (datetime.datetime): Start time of earliest forecast
+        t1 (datetime.datetime): Start time of latest forecast
+        pmin (int): Lowest precision forecast (sbits)
+        pmax (int): Highest precision forecast (sbits)
+        nt (int): Expected number of timesteps in each forecast
+
+    Returns:
+        cubes (iris.cube.CubeList): A cubelist containing all forecasts with
+            cubes merged over new coordinates of start time and precision
+    """
     all_cubes = iris.cube.CubeList()
-
     for time in pd.date_range(t0, t1, freq='MS'):
-        print time
+        print(time)
 
-        yyyymmddhh = time.strftime('%Y%m%d%H')
+        if prefix == 'yyyymmddhh':
+            filename = path + time.strftime('%Y%m%d%H')
+        else:
+            filename = path + prefix
 
         for n in range(pmin, pmax+1):
-            cubes = iris.load(datadir + 'yyyymmddhh' + '_p' + str(n) + '.nc')
+            cubes = iris.load(filename + '_' + str(n) + '.nc')
             coord = iris.coords.AuxCoord(points=n, long_name='precision')
             for cube in cubes:
                 cube.add_aux_coord(coord)
@@ -29,13 +59,12 @@ def main():
             t0_dt(cubes)
 
             for cube in cubes:
-                all_cubes.append(cube)
+                if len(cube.coord('forecast_period').points) == nt:
+                    all_cubes.append(cube)
 
     cubes = all_cubes.merge()
-    print cubes
-    iris.save(cubes, datadir + str(t0.year) + '-' + str(t1.year) + '_' +
-              str(pmin) + '-' + str(pmax) + '.nc')
-    return
+
+    return cubes
 
 
 def t0_dt(cubes):
