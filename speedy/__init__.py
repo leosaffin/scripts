e660feb4418f5ea5023e224a36635a24294b5671
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2018-08-30 11:21:17 +0100

    Added PlotParameters for physics schemes in SPEEDY

--- speedy/__init__.py
+++ speedy/__init__.py
@@ -1,27 +1,16 @@
-from iris.analysis import MEAN, STD_DEV
 from mymodule.user_variables import datadir, plotdir
+from mymodule.plot.util import PlotParameter
 
 datadir = datadir + 'speedy/'
 plotdir = plotdir + 'speedy/'
 
-
-def ensemble_std_dev(cube, weights=None):
-    std = cube.collapsed(['ensemble_member'], STD_DEV)
-    std = std.collapsed(['longitude', 'latitude'],
-                        MEAN, weights=weights)
-
-    return std
-
-
-def rms_diff(z1, z2, weights=None):
-    rms = ((z1-z2)**2).collapsed(['longitude', 'latitude'],
-                                 MEAN, weights=weights)**0.5
-
-    return rms
-
-
-def mean_diff(z1, z2, weights=None):
-    mean = (z1-z2).collapsed(['longitude', 'latitude'],
-                             MEAN, weights=weights)
-
-    return mean
+physics_schemes = {
+    'Physics': PlotParameter(color='k', linestyle='-', idx=0),
+    'Convection': PlotParameter(color='b', linestyle='--', idx=1),
+    'Condensation': PlotParameter(color='b', linestyle='-', idx=2),
+    'Cloud': PlotParameter(color='grey', linestyle='-', idx=3),
+    'Short-Wave Radiation': PlotParameter(color='r', linestyle='--', idx=4),
+    'Long-Wave Radiation': PlotParameter(color='r', linestyle='-', idx=5),
+    'Surface Fluxes': PlotParameter(color='g', linestyle='--', idx=6),
+    'Vertical Diffusion': PlotParameter(color='g', linestyle='-', idx=7),
+}
