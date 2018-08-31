b70deccdad8c5853e7fbb1be28c31012b7f5ede4
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2018-08-31 14:55:37 +0100

    Commit file for previous commit

--- models/speedy/__init__.py
+++ models/speedy/__init__.py
@@ -5,12 +5,13 @@ datadir = datadir + 'speedy/'
 plotdir = plotdir + 'speedy/'
 
 physics_schemes = {
-    'Physics': PlotParameter(color='k', linestyle='-', idx=0),
-    'Convection': PlotParameter(color='b', linestyle='--', idx=1),
-    'Condensation': PlotParameter(color='b', linestyle='-', idx=2),
-    'Cloud': PlotParameter(color='grey', linestyle='-', idx=3),
-    'Short-Wave Radiation': PlotParameter(color='r', linestyle='--', idx=4),
-    'Long-Wave Radiation': PlotParameter(color='r', linestyle='-', idx=5),
-    'Surface Fluxes': PlotParameter(color='g', linestyle='--', idx=6),
-    'Vertical Diffusion': PlotParameter(color='g', linestyle='-', idx=7),
+    'Physics':              PlotParameter(color='k',    linestyle='-',  idx=0),
+    'Convection':           PlotParameter(color='b',    linestyle='--', idx=1),
+    'Condensation':         PlotParameter(color='b',    linestyle='-',  idx=2),
+    'Cloud':                PlotParameter(color='grey', linestyle='-',  idx=3),
+    'Short-Wave Radiation': PlotParameter(color='r',    linestyle=':',  idx=4),
+    'Long-Wave Radiation':  PlotParameter(color='r',    linestyle='--', idx=5),
+    'Surface Fluxes':       PlotParameter(color='g',    linestyle='--', idx=6),
+    'Vertical Diffusion':   PlotParameter(color='g',    linestyle='-',  idx=7),
+    'SPPT':                 PlotParameter(color='k',    linestyle='--', idx=8),
 }
