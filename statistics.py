7705c52acc685e1c24b14ee3383c843487642278
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2018-11-22 15:58:44 +0000

    Separate mean and rms function in statistics

--- statistics.py
+++ statistics.py
@@ -8,28 +8,37 @@ from iris.analysis import MEAN, STD_DEV, COUNT
 from iris.analysis.cartography import cosine_latitude_weights
 
 
+def global_mean(cube):
+    weights = cosine_latitude_weights(cube)
+    mean = cube.collapsed(['longitude', 'latitude'], MEAN, weights=weights)
+
+    return mean
+
+
+def root_mean_square(cube):
+    weights = cosine_latitude_weights(cube)**2
+    rms = (cube**2).collapsed(['longitude', 'latitude'],
+                              MEAN, weights=weights)**0.5
+
+    return rms
+
+
 def ensemble_std_dev(cube):
     std = cube.collapsed(['ensemble_member'], STD_DEV)
-    weights = cosine_latitude_weights(std)
-    std = std.collapsed(['longitude', 'latitude'],
-                        MEAN, weights=weights)
+    std = root_mean_square(std)
 
     return std
 
 
 def rms_diff(zfc, zref):
-    weights = cosine_latitude_weights(zfc)
-    rms = ((zfc-zref)**2).collapsed(['longitude', 'latitude'],
-                                    MEAN, weights=weights)**0.5
+    rms = root_mean_square(zfc - zref)
     rms.rename('RMS error in {}'.format(zfc.name()))
 
     return rms
 
 
 def mean_diff(zfc, zref):
-    weights = cosine_latitude_weights(zfc)
-    mean = (zfc-zref).collapsed(['longitude', 'latitude'],
-                                MEAN, weights=weights)
+    mean = global_mean(zfc - zref)
     mean.rename('Mean error in {}'.format(zfc.name()))
 
     return mean
