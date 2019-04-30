151d167c425a2b7e8d1375877ea010057631f7aa
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2019-04-30 16:38:03 +0100

    Added second panel for overlapping coefficient with reduced precision parametrizations

--- projects/ithaca/rp_physics/fig3_rp_ensemble_overlap.py
+++ projects/ithaca/rp_physics/fig3_rp_ensemble_overlap.py
@@ -4,30 +4,81 @@ Plot the overlapping coefficient between ensemble forecasts
 
 import matplotlib.pyplot as plt
 import iris
-from irise.plot.util import legend
+import iris.coords
+import iris.plot as iplt
+from iris.analysis import MIN, MAX, MEAN
+from irise.plot.util import legend, multilabel
+
 from myscripts.models.speedy import datadir, physics_schemes
 
 
 def main():
     path = datadir + 'stochastic/ensembles/'
 
+    # Get overlap from exchanged ensembles
+    ovl_range = iris.load(path + 'ovl_perturbed_??.nc')
+
+    for n, cube in enumerate(ovl_range):
+        cube.add_aux_coord(iris.coords.AuxCoord(n, long_name='ensemble'))
+
+    ovl_range = ovl_range.merge_cube()
+    ovl_range.coord('forecast_period').convert_units('days')
+
+    ovl_min = ovl_range.collapsed('ensemble', MIN)
+    ovl_max = ovl_range.collapsed('ensemble', MAX)
+    ovl_mean = ovl_range.collapsed('ensemble', MEAN)
+
+    # Two panels
+    fig, axes = plt.subplots(nrows=1, ncols=1, sharey='row', figsize=[8, 5])
+
+    # Panel 1 -
+    plt.axes(axes[0])
+    multilabel(axes[0], 0, 0.01)
+    plt.fill_between(ovl_range.coord('forecast_period').points,
+                     ovl_min.data, ovl_max.data, color='grey')
+
     files = [
-        ('overlap_52_52.nc', 'Physics (52 sbit)', '-', 'purple'),
-        ('overlap_52_23.nc', 'Physics (23 sbit)', '-', 'k'),
-        ('overlap_52_10.nc', 'Physics (10 sbit)', '--', 'k'),
-        ('overlap_52_8.nc', 'Physics (8 sbit)', ':', 'k'),
-        ('overlap_52_cnv8.nc', 'Convection (8 sbit)', '--', 'b'),
-        ('overlap_52_sflx8.nc', 'Surface Fluxes (8 sbit)', '--', 'grey'),
-        ('overlap_52_vdif8.nc', 'Vertical Diffusion (8 sbit)', '-', 'g')
+        ('overlap_52_23.nc', '23 sbit', '-', 'k'),
+        ('overlap_52_10.nc', '10 sbit', '--', 'k'),
+        ('overlap_52_8.nc', '8 sbit', ':', 'k'),
+        ('overlap_52_adj8.nc', '8 sbit, Fixed', '--', 'y')
         ]
 
     for filename, label, linestyle, color in files:
         overlap = iris.load_cube(path + filename)
-
-        plp = physics_schemes[label.split(' (')[0]]
-        plp.plot(overlap, label=label, linestyle=linestyle, color=color)
+        overlap.coord('forecast_period').convert_units('days')
+        iplt.plot(overlap, label=label, color=color, linestyle=linestyle)
 
     legend()
+
+    # Panel 2
+    plt.axes(axes[1])
+    multilabel(axes[1], 1, 0.01)
+    plt.fill_between(ovl_range.coord('forecast_period').points,
+                     ovl_min.data, ovl_max.data, color='grey')
+
+    files = [
+        ('overlap_52_cnv8.nc', 'Convection'),
+        ('overlap_52_cond8.nc', 'Condensation'),
+        ('overlap_52_swrad8.nc', 'Short-Wave Radiation'),
+        ('overlap_52_lwrad8.nc', 'Long-Wave Radiation'),
+        ('overlap_52_sflx8.nc', 'Surface Fluxes'),
+        ('overlap_52_vdif8.nc', 'Vertical Diffusion'),
+        ]
+
+    for filename, label in files:
+        overlap = iris.load_cube(path + filename)
+        overlap.coord('forecast_period').convert_units('days')
+
+        plp = physics_schemes[label]
+        plp.plot(overlap, label=label)
+
+    legend()
+    axes.set_ylabel('Overlapping Coefficient')
+    axes.set_xlabel('Forecast Lead Time [days]')
+    fig.text(0.5, 0.01, 'Forecast Lead Time [days]', ha='center')
+
+
     plt.show()
     return
 
