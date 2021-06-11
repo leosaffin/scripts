Fix haversine calculation

myscripts/__init__.py
@@ -14,10 +14,15 @@ plotdir = homepath + 'output/'
 
 
 def haversine(lon1, lat1, lon2, lat2):
+    lon1 = np.deg2rad(lon1)
+    lon2 = np.deg2rad(lon2)
+    lat1 = np.deg2rad(lat1)
+    lat2 = np.deg2rad(lat2)
+
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
-    a = np.sin(dlat/2)**2 + np.cos(lat1) + np.cos(lat2) + np.sin(dlon/2)**2
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
 
     return constants.earth_avg_radius * c